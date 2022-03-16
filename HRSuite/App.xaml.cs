bb8817a0bebfd08e133f6ab7d6b0138ff7cd4b44
CrimsonOrion <crimsonorion@gmail.com>
2022-03-16 17:08:49 -0400

    v2022.3.16.1 - Initial Publish

---
+++ HRSuite/App.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+using About.Module;
+
+using Codes.Module;
+
+using ControlzEx.Theming;
+
+using Employee.Module;
+
+using HRSuite.Core;
+using HRSuite.Core.ConfigurationModels;
+using HRSuite.Core.Processors;
+using HRSuite.Core.Reporting;
+
+using JobCode.Module;
+
+using Library.NET.DataAccess;
+using Library.NET.Events;
+using Library.NET.Logging;
+using Library.NET.Mailer;
+
+using MahApps.Metro.Controls.Dialogs;
+
+using Microsoft.Extensions.Configuration;
+
+using Prism.Ioc;
+using Prism.Modularity;
+
+using Reports.Module;
+
+using Requisition.Module;
+
+namespace HRSuite;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override Window CreateShell() => Container.Resolve<MainWindow>();
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;
+        ThemeManager.Current.SyncTheme();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        // Set up the custom logger to go to Desktop
+        ICustomLogger logger = new CustomLogger(
+            new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "HR Suite Error.log")),
+            false,
+            LogLevel.Warning);
+
+        // Set up the configuration using the appSettings.json file.
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", false, true)
+            .Build();
+
+        GlobalConfig.FileLocationsConfiguration = new()
+        {
+            LetterheadBwDfs = configuration["File Locations:Letterhead-BW-DFS"] ?? "",
+            LetterheadBwHardcode = configuration["File Locations:Letterhead-BW-Hardcode"] ?? "",
+        };
+
+        GlobalConfig.EmailSettingsConfiguration = new()
+        {
+            SmtpServer = configuration["Email Settings:Smtp Server"] ?? "",
+            AdminEmail = configuration["Email Settings:Admin Email"] ?? "",
+            AdminText = configuration["Email Settings:Admin Text"] ?? ""
+        };
+
+        containerRegistry
+                .RegisterInstance(logger)
+                .RegisterInstance<IDialogCoordinator>(new DialogCoordinator())
+
+                .Register<IEmailer, FluentEmailerMailKit>()
+                .Register<IErrorHandler, ErrorHandler>()
+                .Register<ISqlDataAccess, MsSqlDataAccess>()
+                .Register<IDataProcessor, DataProcessor>()
+                .Register<IWorksheet, Worksheet>()
+                .Register<ILetter, Letter>()
+
+                .RegisterSingleton<ISendMessageEvent, SendMessageEvent>()
+                ;
+    }
+
+    protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) => moduleCatalog
+        .AddModule<AboutModule>()
+        .AddModule<CodesModule>()
+        .AddModule<EmployeeModule>()
+        .AddModule<JobCodeModule>()
+        .AddModule<ReportsModule>()
+        .AddModule<RequisitionModule>()
+        ;
+}
---
+++ HRSuite/MainWindowViewModel.cs
@@ -0,0 +1,133 @@
+﻿using HRSuite.Core;
+
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace HRSuite;
+public class MainWindowViewModel : BindableBase
+{
+    private readonly IRegionManager _regionManager;
+
+    #region Main Window Properties
+
+    public static string Title => $"HR Suite v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+
+    #endregion
+
+    #region Delegate Commands
+
+    #region Navigation
+
+    #region Employees
+
+    public DelegateCommand EditEmployeeScreenCommand => new(EditEmployeeScreen);
+    public DelegateCommand TerminateEmployeeScreenCommand => new(TerminateEmployeeScreen);
+
+    private void EditEmployeeScreen() => Navigate(KnownViewNames.EditEmployeeView);
+    private void TerminateEmployeeScreen() => Navigate(KnownViewNames.TerminateEmployeeView);
+
+    #endregion
+
+    #region Requisitions
+
+    public DelegateCommand CreateRequisitionScreenCommand => new(CreateRequisitionScreen);
+    public DelegateCommand EditRequisitionScreenCommand => new(EditRequisitionScreen);
+
+    private void CreateRequisitionScreen() => Navigate(KnownViewNames.AddRequisitionView);
+    private void EditRequisitionScreen() => Navigate(KnownViewNames.EditRequisitionView);
+
+    #endregion
+
+    #region Code Data
+
+    #region Job Codes
+
+    public DelegateCommand AddJobCodeScreenCommand => new(AddJobCodeScreen);
+    public DelegateCommand EditJobCodeScreenCommand => new(EditJobCodeScreen);
+
+    private void AddJobCodeScreen() => Navigate(KnownViewNames.AddJobCodeView);
+    private void EditJobCodeScreen() => Navigate(KnownViewNames.EditJobCodeView);
+
+    #endregion
+
+    #region Other Codes
+
+    public DelegateCommand AddCodeScreenCommand => new(AddCodeScreen);
+    public DelegateCommand EditCodeScreenCommand => new(EditCodeScreen);
+
+    private void AddCodeScreen() => Navigate(KnownViewNames.AddCodeView);
+    private void EditCodeScreen() => Navigate(KnownViewNames.EditCodeView);
+
+    #endregion
+
+    #endregion
+
+    #region Reports
+
+    #region Employee
+
+    public DelegateCommand AgeListScreenCommand => new(AgeListScreen);
+    public DelegateCommand BirthdayListScreenCommand => new(BirthdayListScreen);
+    public DelegateCommand CDLReportScreenCommand => new(CDLReportScreen);
+    public DelegateCommand EmployeeChecklistScreenCommand => new(EmployeeChecklistScreen);
+    public DelegateCommand EmployeeListScreenCommand => new(EmployeeListScreen);
+    public DelegateCommand PromotionsScreenCommand => new(PromotionsScreen);
+    public DelegateCommand RequisitionProfileScreenCommand => new(RequisitionProfileScreen);
+    public DelegateCommand SafetyMeetingAttendanceListScreenCommand => new(SafetyMeetingAttendanceListScreen);
+    public DelegateCommand ServiceListScreenCommand => new(ServiceListScreen);
+
+    private void AgeListScreen() => Navigate(KnownViewNames.AgeListReportView);
+    private void BirthdayListScreen() => Navigate(KnownViewNames.BirthdayListReportView);
+    private void CDLReportScreen() => Navigate(KnownViewNames.CDLReportView);
+    private void EmployeeChecklistScreen() => Navigate(KnownViewNames.EmployeeChecklistReportView);
+    private void EmployeeListScreen() => Navigate(KnownViewNames.EmployeeListReportView);
+    private void PromotionsScreen() => Navigate(KnownViewNames.PromotionsReportView);
+    private void RequisitionProfileScreen() => Navigate(KnownViewNames.RequisitionProfileReportView);
+    private void SafetyMeetingAttendanceListScreen() => Navigate(KnownViewNames.SafetyMeetingAttendanceListReportView);
+    private void ServiceListScreen() => Navigate(KnownViewNames.ServiceListReportView);
+
+    #endregion
+
+    #region EEO
+
+    public DelegateCommand NewHireHeadcountDetailScreenCommand => new(NewHireHeadcountDetailScreen);
+    public DelegateCommand NewHireHeadcountSummaryScreenCommand => new(NewHireHeadcountSummaryScreen);
+    public DelegateCommand EEORequisitionProfileScreenCommand => new(EEORequisitionProfileScreen);
+    public DelegateCommand TerminationDetailScreenCommand => new(TerminationDetailScreen);
+    public DelegateCommand TerminationSummaryScreenCommand => new(TerminationSummaryScreen);
+
+    private void NewHireHeadcountDetailScreen() => Navigate(KnownViewNames.NewHireHeadcountDetailReportView);
+    private void NewHireHeadcountSummaryScreen() => Navigate(KnownViewNames.NewHireHeadcountSummaryReportView);
+    private void EEORequisitionProfileScreen() => Navigate(KnownViewNames.EEORequisitionProfileReportView);
+    private void TerminationDetailScreen() => Navigate(KnownViewNames.TerminationDetailReportView);
+    private void TerminationSummaryScreen() => Navigate(KnownViewNames.TerminationSummaryReportView);
+
+    #endregion
+
+    #endregion
+
+    #region About
+
+    public DelegateCommand AboutScreenCommand => new(AboutScreen);
+
+    private void AboutScreen() => Navigate(KnownViewNames.AboutView);
+
+    #endregion
+
+    #endregion
+
+    #endregion
+
+    #region Constructor
+
+    public MainWindowViewModel(IRegionManager regionManager) => _regionManager = regionManager;
+
+    #endregion
+
+    #region Private Methods
+
+    private void Navigate(string navigationPath, NavigationParameters navigationParameters = null) => _regionManager.RequestNavigate(KnownRegionNames.MainRegion, navigationPath, navigationParameters);
+
+    #endregion
+}
